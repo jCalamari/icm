6715afb48fae373ab8db6f7ad8042871f86ccf9e
Piotr Fras <piotrek.fras@gmail.com>
2015-07-21 17:14:57 +0100

    rich config value

--- app/models/InjectableConfigModule.scala
+++ app/models/InjectableConfigModule.scala
@@ -21,30 +21,42 @@ class InjectableConfigModule(basePath: Option[String] = None) extends Module {
   }
 
   private def bind(key: String, value: ConfigValue): List[Binding[_]] = value.valueType() match {
-    case ConfigValueType.OBJECT =>
-      bindObject(key, value) ::: bindObjectBody(key, value.asInstanceOf[java.util.Map[String, ConfigValue]].asScala.toMap)
-    case ConfigValueType.LIST =>
-      bindListBody(key, value.asInstanceOf[java.util.List[ConfigValue]].asScala.toList)
+    case ConfigValueType.OBJECT => bindObject(key, value) ::: bindObjectBody(key, value.asMap())
+    case ConfigValueType.LIST => bindListBody(key, value.asList())
     case ConfigValueType.NUMBER => bindNumber(key, value)
-    case ConfigValueType.BOOLEAN =>
-      List(bind[java.lang.Boolean].qualifiedWith(Configs.config(key)).toInstance(value.unwrapped().asInstanceOf[java.lang.Boolean]))
+    case ConfigValueType.BOOLEAN => bindBoolean(value.asJavaBoolean(), key)
     case ConfigValueType.NULL => Nil
-    case ConfigValueType.STRING =>
-      List(bind[String].qualifiedWith(Configs.config(key)).toInstance(value.unwrapped().toString))
+    case ConfigValueType.STRING => List(bind[String].qualifiedWith(Configs.config(key)).toInstance(value.unwrapped().toString))
   }
 
   private def bindNumber(key: String, value: ConfigValue): List[Binding[_]] = value.unwrapped() match {
     case i: java.lang.Integer => List(
-      bind[java.lang.Integer].qualifiedWith(Configs.config(key)).toInstance(i),
-      bind[java.lang.Long].qualifiedWith(Configs.config(key)).toInstance(java.lang.Long.valueOf(i.longValue())),
-      bind[java.lang.Double].qualifiedWith(Configs.config(key)).toInstance(java.lang.Double.valueOf(i.doubleValue()))
+      bindInteger(i, key),
+      bindLong(i.longValue(), key),
+      bindDouble(i.doubleValue(), key)
     )
     case l: java.lang.Long => List(
-      bind[java.lang.Long].qualifiedWith(Configs.config(key)).toInstance(l),
-      bind[java.lang.Double].qualifiedWith(Configs.config(key)).toInstance(java.lang.Double.valueOf(l.doubleValue()))
+      bindLong(l, key),
+      bindDouble(l.doubleValue(), key)
     )
-    case d: java.lang.Double => List(bind[java.lang.Double].qualifiedWith(Configs.config(key)).toInstance(d))
+    case d: java.lang.Double => List(bindDouble(d, key))
     case _ => Nil
+  }
+
+  private def bindBoolean(b: java.lang.Boolean, key: String): List[Binding[_]] = {
+    List(bind[java.lang.Boolean].qualifiedWith(Configs.config(key)).toInstance(b))
+  }
+
+  private def bindInteger(i: java.lang.Integer, key: String): Binding[_] = {
+    bind[java.lang.Integer].qualifiedWith(Configs.config(key)).toInstance(i)
+  }
+
+  private def bindLong(l: java.lang.Long, key: String): Binding[_] = {
+    bind[java.lang.Long].qualifiedWith(Configs.config(key)).toInstance(l)
+  }
+
+  private def bindDouble(d: java.lang.Double, key: String): Binding[_] = {
+    bind[java.lang.Double].qualifiedWith(Configs.config(key)).toInstance(d)
   }
 
   private def bindListBody(key: String, values: List[ConfigValue], index: => Int = 0): List[Binding[_]] = values match {
@@ -54,13 +66,25 @@ class InjectableConfigModule(basePath: Option[String] = None) extends Module {
 
   private def bindObject(key: String, value: ConfigValue): List[Binding[_]] = {
     List(
-      bind[Configuration].qualifiedWith(Configs.config(key)).toInstance(Configuration(value.asInstanceOf[ConfigObject].toConfig)),
-      bind[play.Configuration].qualifiedWith(Configs.config(key)).toInstance(new play.Configuration(value.asInstanceOf[ConfigObject].toConfig))
+      bind[Configuration].qualifiedWith(Configs.config(key)).toInstance(value.asConfig()),
+      bind[play.Configuration].qualifiedWith(Configs.config(key)).toInstance(value.asJavaConfig())
     )
   }
 
   private def bindObjectBody(key: String, value: Map[String, ConfigValue]): List[Binding[_]] = value.flatMap { case (mKey, mVal) =>
     bind(key + "." + mKey, mVal)
   }.toList
+
+  private implicit class RichConfigValue(value: ConfigValue) {
+    def asMap(): Map[String, ConfigValue] = value.asInstanceOf[java.util.Map[String, ConfigValue]].asScala.toMap
+
+    def asList(): List[ConfigValue] = value.asInstanceOf[java.util.List[ConfigValue]].asScala.toList
+
+    def asConfig(): Configuration = Configuration(value.asInstanceOf[ConfigObject].toConfig)
+
+    def asJavaConfig(): play.Configuration = new play.Configuration(value.asInstanceOf[ConfigObject].toConfig)
+
+    def asJavaBoolean(): java.lang.Boolean = value.unwrapped().asInstanceOf[java.lang.Boolean]
+  }
 
 }
