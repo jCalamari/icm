b6d4ed197e12c4cdc874328625e606c3b622a35e
Piotr Fras <piotr.fras@xendpay.com>
2015-07-21 15:19:43 +0100

    icm spec

--- app/models/InjectableConfigModule.scala
+++ app/models/InjectableConfigModule.scala
@@ -1,24 +1,24 @@
 package models
 
-import com.typesafe.config.{ConfigObject, ConfigValue, ConfigValueType}
+import com.typesafe.config.{ConfigValue, ConfigValueType}
 import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
 
 import scala.collection.JavaConverters._
 import scala.language.postfixOps
 
-class InjectableConfigModule extends Module {
+class InjectableConfigModule(basePath: Option[String] = None) extends Module {
 
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
-    configuration.entrySet.toSeq.flatMap { case (key, value) =>
+    basePath.flatMap(configuration.getConfig).getOrElse(configuration).entrySet.toSeq.flatMap { case (key, value) =>
       bind(key, value)
     }
   }
 
   private def bind(key: String, value: ConfigValue): List[Binding[_]] = {
     value.valueType() match {
-      case ConfigValueType.OBJECT => bindObject(key, value) :: bindObjectBody(key, value.asInstanceOf[java.util.Map[String, ConfigValue]].asScala.toMap)
-      case ConfigValueType.LIST => bindListBody(key, value.asInstanceOf[java.util.List[ConfigValue]].asScala.toList)
+      case ConfigValueType.OBJECT => Nil
+      case ConfigValueType.LIST => bindList(key, value.asInstanceOf[java.util.List[ConfigValue]].asScala.toList)
       case ConfigValueType.NUMBER => bindNumber(key, value)
       case ConfigValueType.BOOLEAN => List(bind[java.lang.Boolean].qualifiedWith(Configs.config(key)).toInstance(value.unwrapped().asInstanceOf[java.lang.Boolean]))
       case ConfigValueType.NULL => Nil
@@ -29,28 +29,25 @@ class InjectableConfigModule extends Module {
   private def bindNumber(key: String, value: ConfigValue): List[Binding[_]] = {
     val qualifier = Configs.config(key)
     value.unwrapped() match {
+      case i: java.lang.Integer => List(
+        bind[java.lang.Integer].qualifiedWith(qualifier).toInstance(i),
+        bind[java.lang.Long].qualifiedWith(qualifier).toInstance(java.lang.Long.valueOf(i.longValue())),
+        bind[java.lang.Double].qualifiedWith(qualifier).toInstance(java.lang.Double.valueOf(i.doubleValue()))
+      )
+      case l: java.lang.Long => List(
+        bind[java.lang.Long].qualifiedWith(qualifier).toInstance(l),
+        bind[java.lang.Double].qualifiedWith(qualifier).toInstance(java.lang.Double.valueOf(l.doubleValue()))
+      )
       case d: java.lang.Double => List(bind[java.lang.Double].qualifiedWith(qualifier).toInstance(d))
-      case i: java.lang.Integer => List(bind[java.lang.Integer].qualifiedWith(qualifier).toInstance(i))
-      case l: java.lang.Long => List(bind[java.lang.Long].qualifiedWith(qualifier).toInstance(l))
       case _ => Nil
     }
   }
 
-  private def bindListBody(key: String, values: List[ConfigValue], index: Int = 0): List[Binding[_]] = {
+  private def bindList(key: String, values: List[ConfigValue], index: Int = 0): List[Binding[_]] = {
     values match {
       case Nil => Nil
-      case head :: tail => bind(key + "." + index, head) ::: bindListBody(key, tail, index + 1)
+      case head :: tail => bind(key + "." + index, head) ::: bindList(key, tail, index + 1)
     }
-  }
-
-  private def bindObject(key: String, value: ConfigValue): Binding[_] = {
-    bind[Configuration].qualifiedWith(Configs.config(key)).toInstance(Configuration(value.asInstanceOf[ConfigObject].toConfig))
-  }
-
-  private def bindObjectBody(key: String, value: Map[String, ConfigValue]): List[Binding[_]] = {
-    value.flatMap { case (mKey, mVal) =>
-      bind(key + "." + mKey, mVal)
-    }.toList
   }
 
 }
---
+++ test/models/InjectableConfigModuleSpec.scala
@@ -0,0 +1,128 @@
+package models
+
+import com.google.inject.ConfigurationException
+import org.specs2.mutable.Specification
+import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
+import play.api.inject.{BindingKey, BuiltinModule, QualifierInstance}
+import play.api.{ApplicationLoader, Configuration, Environment}
+
+object InjectableConfigModuleSpec extends Specification {
+
+  "InjectableConfigModule" should {
+
+    "inject numbers" in {
+      val injector = playInjector()
+
+      // Int
+      injector.instanceOf(binding(classOf[Int], "int")) must equalTo[Int](121535)
+      injector.instanceOf(binding(classOf[Long], "int")) must equalTo[Long](121535L)
+      injector.instanceOf(binding(classOf[Double], "int")) must equalTo[Double](121535.00)
+
+      // Long
+      injector.instanceOf(binding(classOf[Int], "long")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "long")) must equalTo[Long](1768619760268566049L)
+      injector.instanceOf(binding(classOf[Double], "long")) must equalTo[Double](1768619760268566049.00)
+
+      // Double
+      injector.instanceOf(binding(classOf[Int], "double")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "double")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Double], "double")) must equalTo[Double](2.5D)
+    }
+
+    "inject booleans" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[Boolean], "false")) must equalTo[Boolean](false)
+      injector.instanceOf(binding(classOf[Boolean], "true")) must equalTo[Boolean](true)
+    }
+
+    "inject strings" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[String], "nonEmptyString")) must equalTo[String]("character sequence")
+      injector.instanceOf(binding(classOf[String], "emptyString")) must equalTo[String]("")
+    }
+
+    "inject list of ints" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[Int], "ints.0")) must equalTo[Int](523969)
+      injector.instanceOf(binding(classOf[Long], "ints.0")) must equalTo[Long](523969L)
+      injector.instanceOf(binding(classOf[Double], "ints.0")) must equalTo[Double](523969D)
+
+      injector.instanceOf(binding(classOf[Int], "ints.1")) must equalTo[Int](177618)
+      injector.instanceOf(binding(classOf[Long], "ints.1")) must equalTo[Long](177618L)
+      injector.instanceOf(binding(classOf[Double], "ints.1")) must equalTo[Double](177618D)
+
+      injector.instanceOf(binding(classOf[Double], "ints.2")) must throwAn[ConfigurationException]
+    }
+
+    "inject list of longs" in {
+      val injector = playInjector()
+
+      injector.instanceOf(binding(classOf[Int], "longs.0")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "longs.0")) must equalTo[Long](5272471683069627553L)
+      injector.instanceOf(binding(classOf[Double], "longs.0")) must equalTo[Double](5272471683069627553D)
+
+      injector.instanceOf(binding(classOf[Int], "longs.1")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "longs.1")) must equalTo[Long](789875541540841016L)
+      injector.instanceOf(binding(classOf[Double], "longs.1")) must equalTo[Double](789875541540841016D)
+
+      injector.instanceOf(binding(classOf[Double], "longs.2")) must throwAn[ConfigurationException]
+    }
+
+    "inject list of doubles" in {
+      val injector = playInjector()
+
+      injector.instanceOf(binding(classOf[Int], "doubles.0")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "doubles.0")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Double], "doubles.0")) must equalTo[Double](3.5D)
+
+      injector.instanceOf(binding(classOf[Int], "doubles.1")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "doubles.1")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Double], "doubles.1")) must equalTo[Double](4.5D)
+
+      injector.instanceOf(binding(classOf[Double], "doubles.2")) must throwAn[ConfigurationException]
+    }
+
+    "inject nested non empty config" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[String], "nonEmptyConfig.innerKey")) must equalTo[String]("innerValue")
+    }
+
+    "not inject empty config" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[Configuration], "emptyConfig")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Configuration], "innerEmptyConfig")) must throwAn[ConfigurationException]
+    }
+
+    "not inject nulls" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[Int], "nullRef")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "nullRef")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Double], "nullRef")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[String], "nullRef")) must throwAn[ConfigurationException]
+    }
+
+    "not inject empty list" in {
+      val injector = playInjector()
+      injector.instanceOf(binding(classOf[Int], "emptyList")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Long], "emptyList")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[Double], "emptyList")) must throwAn[ConfigurationException]
+      injector.instanceOf(binding(classOf[String], "emptyList")) must throwAn[ConfigurationException]
+    }
+
+  }
+
+  def fakeContext = ApplicationLoader.createContext(Environment.simple())
+
+  def binding[T](clazz: Class[T], qualifier: String) = {
+    // @Config("qualifier")
+    new BindingKey(clazz, Some(QualifierInstance(Configs.config(qualifier))))
+  }
+
+  def playInjector() = {
+    val builder = new GuiceApplicationBuilder().load(new BuiltinModule, new InjectableConfigModule(Some("icm")))
+    val loader = new GuiceApplicationLoader(builder)
+    val app = loader.load(fakeContext)
+    app.injector
+  }
+
+}
